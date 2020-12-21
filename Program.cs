23031d3ae11bc68f854279b056ebf3e8f2dc582a
abasov-albert <abasovalik2001@gmail.com>
2020-12-21 21:43:08 +0300

    Add stopwatch and refactor output

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace SortLab
 {
@@ -8,64 +10,93 @@ namespace SortLab
     {
         static void Main(string[] args)
         {
-            List<int> numbers1 = new List<int>() { };
-            var rand = new Random();
+            SelectionSort();
+            RadixSort();
+        }        
 
-            for (int i = 0; i < 100; i++)
-            {
-                numbers1.Add(rand.Next((100)));
-            }
-            
-            for (int i = 0; i < numbers1.Count; i++)
-            {
-                Console.WriteLine(numbers1[i]);
-            }
-            
-            Console.WriteLine();
-            Console.WriteLine("Sorted:");
-
-            SelectionSort(numbers1);
-
-            List<int>[] numbers2 = new List<int>[10];
-
-            for (int i = 0; i < numbers2.Length; i++)
-            {
-                numbers2[i] = new List<int>();
-            }
-
-            int count = 0;
-            
-            for (int i = 0; i < numbers1.Count; i++)
-            {
-                if (numbers1[i] % 10 < numbers1[i + 1] % 10)
-                {
-                    numbers2[count].Add(numbers1[i]);
-                    
-                    if (numbers1[i] > numbers2[count][0])
-                    {
-                        count++;
-                        numbers2[count].Add(numbers1[i]);
-                        numbers1.Remove(numbers1[i]);
-                    }
-                    numbers1.Remove(numbers1[i]);
-                    i = -1;
-                }
-            }
-        }
-
-        public static void SelectionSort(List<int> numbers)
+        public static void SelectionSort()
         {
+            List<int> numbers = new List<int>() { };
+            var rand = new Random();
+            Console.Write("Enter the amount of elements: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Before sorting:");
+            for (int i = 0; i < n; i++)
+            {
+                numbers.Add(rand.Next(n));
+                Console.Write(numbers[i] + " ");
+            }
+            
             List<int> sortedNumbers = new List<int>();
-
+            
+            // sorting started
+            var watch = Stopwatch.StartNew();
             while (numbers.Count > 0)
             {
                 sortedNumbers.Add(numbers.Min());
                 numbers.Remove(numbers.Min());
             }
-
+            
+            watch.Stop();
+            Console.WriteLine();
+            Console.WriteLine($"Sorting took {watch.Elapsed}");
+            Console.WriteLine();
+            Console.WriteLine("Sorted with selection sort:");
             for (int i = 0; i < sortedNumbers.Count; i++)
             {
-                Console.WriteLine(sortedNumbers[i]);
+                Console.Write(sortedNumbers[i] + " ");
+            }
+        }
+
+        public static void RadixSort()
+        {
+            List<int> numbers = new List<int>() { };
+            var rand = new Random();
+            Console.WriteLine();
+            Console.Write("Enter the amount of elements: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Before sorting:");
+            for (int i = 0; i < n; i++)
+            {
+                numbers.Add(rand.Next(n));
+                Console.Write(numbers[i] + " ");
+            }
+
+            var watch = Stopwatch.StartNew();
+
+            List<int>[] numbers2 = new List<int>[10];
+            
+            int maxRadix = numbers.Max().ToString().Length;
+            
+            for(int j = 0; j < maxRadix; j++) 
+            {
+                for (int i = 0; i < numbers2.Length; i++)
+                {
+                    numbers2[i] = new List<int>();
+                }
+                
+                while (numbers.Count > 0)
+                {
+                    numbers2[(numbers[0] / (int)Math.Pow(10, j)) % 10].Add((numbers[0]));
+                    numbers.Remove(numbers[0]);
+                }
+                
+                for (int i = 0; i < numbers2.Length; i++)
+                {
+                    var ints = numbers2[i];
+                    numbers.AddRange(ints);
+                }
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Sorting took: {watch.Elapsed}");
+            Console.WriteLine();
+            Console.WriteLine("Sorted with radix sort:");
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.Write(numbers[i] + " ");
             }
         }
     }
